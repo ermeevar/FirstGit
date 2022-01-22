849821d4e2c4c55130a889899bc697b954d7108a
ermeeva.r@gmail.com <ermeeva.r@gmail.com>
2022-01-22 18:09:40 +0300

    Add sign

--- AppGit/Program.cs
+++ AppGit/Program.cs
@@ -6,7 +6,7 @@ namespace AppGit
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello!");
         }
     }
 }
